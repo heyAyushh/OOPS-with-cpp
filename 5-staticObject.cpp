25594216817570c4a16fa4ef305c8e404b2777fe
Ayush <Ayush-15@live.com>
2017-12-03 13:00:38 +0530

    Program on Page no. 18 & 19  added

---
+++ 5-staticObject.cpp
@@ -0,0 +1,45 @@
+// Refrence is Page no. 18 & 19
+
+/* 
+If you're using Turbo c and it's compilers Do some changes
+write 
+> #include<iostream.h>
+and comment 
+> lines 13 & 14
+*/
+
+// Static initalise all the variables to zero instead of making an constructor for it 
+
+#include <iostream>
+using namespace std;
+
+class sample{
+
+    private:
+        int a; // This is a static data member
+        int b;
+    
+    public:
+        void show_data() //This function prints count then increments it ( postfix increment )
+        {
+            cout<<"a= "<<a<<"b= "<<b<<endl;
+        }
+        void add()
+        {
+            a+=10;
+            b+=20;
+        }       
+};
+
+int main()
+{
+    static sample s; //Made a object static. Static initialises all the data member of the object as zero
+
+    s.show_data(); 
+    s.add();
+    s.show_data();
+
+}
+
+
+
